4717ef6b83c774d9f0b3d4ffaa9290cf2d0eefae
Timir Bhingradiya <95134530+TTimir@users.noreply.github.com>
2024-04-30 22:53:11 +0530

    dashboard

---
+++ JustEat/Admin/Dashboard.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdminMaster.Master" AutoEventWireup="true" CodeBehind="Dashboard.aspx.cs" Inherits="JustEat.Admin.Dashboard" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>Dashboard Page...</h2>
+</asp:Content>
---
+++ JustEat/Users/UserMaster.Master.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JustEat.Users
+{
+    public partial class UserMaster : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }
+            else
+            {
+                // Load Control
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
+
+                // Add control to panel
+                pnlSliderUC.Controls.Add(sliderUserControl);
+            }
+        }
+    }
+}

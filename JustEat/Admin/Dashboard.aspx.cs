05e1834ba289c046242e0a226ae855bf832850dd
Timir Bhingradiya <95134530+TTimir@users.noreply.github.com>
2024-05-11 12:32:18 +0530

    add category

--- JustEat/Admin/Dashboard.aspx.cs
+++ JustEat/Admin/Dashboard.aspx.cs
@@ -11,7 +11,10 @@ namespace JustEat.Admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                Session["breadCrum"] = " ";
+            }
         }
     }
 }
---
+++ JustEat/Connection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace JustEat
+{
+    public class Connection
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        }
+
+        public class Utils
+        {
+            public static bool IsValidExtension(string fileName)
+            {
+                bool IsValid = false;
+                string[] fileExtension = { ".jpg", ".jpeg", ".png" };
+                for (int i = 0; i <= fileExtension.Length - 1; i++)
+                {
+                    if (fileName.Contains(fileExtension[i]))
+                    {
+                        IsValid = true;
+                        break;
+                    }
+                }
+                return IsValid;
+            }
+
+            public static string GetImageUrl(Object url)
+            {
+                string url1 = "";
+                if (string.IsNullOrEmpty(url.ToString()) || url == DBNull.Value)
+                {
+                    url1 = "../Images/No_image.png";
+                }
+                else
+                {
+                    url1 = string.Format("../{0}", url);
+                }
+                return url1;
+            }
+        }
+
+    }
+}

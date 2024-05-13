63173246e46112195a93b4db1eaa8f0cd0d8dceb
Timir Bhingradiya <95134530+TTimir@users.noreply.github.com>
2024-05-13 12:25:18 +0530

    add login

---
+++ JustEat/Users/Registration.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using JustEat.Admin;
+
+namespace JustEat.Users
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter adp;
+        DataTable dt;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            string actionname = string.Empty, imagePath = string.Empty, fileExtension = string.Empty;
+            bool isValidtoExcute = false;
+            int userId = Convert.ToInt32(Request.QueryString["id"]);
+            conn = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("User_Crud", conn);
+            cmd.Parameters.AddWithValue("@Action", userId == 0 ? "INSERT" : "UPDATE");
+            cmd.Parameters.AddWithValue("@userId", userId);
+            cmd.Parameters.AddWithValue("@Name", txtName.Text.Trim());
+            cmd.Parameters.AddWithValue("@Username", txtUserName.Text.Trim());
+            cmd.Parameters.AddWithValue("@Mobile", txtMobile.Text.Trim());
+            cmd.Parameters.AddWithValue("@Email", txtEmail.Text.Trim());
+            cmd.Parameters.AddWithValue("@Address", txtAddress.Text.Trim());
+            cmd.Parameters.AddWithValue("@PostCode", txtPostCode.Text.Trim());
+            cmd.Parameters.AddWithValue("@Password", txtPassword.Text.Trim());
+        }
+    }
+}
